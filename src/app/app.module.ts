d0f119b432c675450183d4a303c9232d05cea00b
vladilin <ilin96inbox@gmail.com>
2019-03-16 18:15:36 +0300

    created Header Recipes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
