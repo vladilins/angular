b080c0e5efe94643c2271d822492c586bb8b257b
vladilin <ilin96inbox@gmail.com>
2019-03-18 15:33:19 +0300

    added router links to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -6,11 +6,7 @@ import { Component, OnInit, Output, EventEmitter } from "@angular/core";
   styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
   constructor() {}
 
   ngOnInit() {}
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
