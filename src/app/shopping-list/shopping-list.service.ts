7dc6fd1b1d77dc5c28cd939387ebfe0793d44f3c
vladilin <ilin96inbox@gmail.com>
2019-03-18 10:47:25 +0300

    shopping list.service AddIngredient method

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable()
-export class ShoppingListService {}
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient("Apple", 5),
+    new Ingredient("Tomatoes", 15)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
