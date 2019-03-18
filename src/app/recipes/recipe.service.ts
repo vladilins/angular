5092a6f14366065ccc2ed0ada2d7180cedfd2874
vladilin <ilin96inbox@gmail.com>
2019-03-18 11:26:04 +0300

    added ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { Injectable, EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      "A test Recipe",
+      "Salad",
       "This is a test",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-E28BB6cRRotXBHjM87qcwyaMg6UGdc_N0J_TBk62jnVmAT07tA"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-E28BB6cRRotXBHjM87qcwyaMg6UGdc_N0J_TBk62jnVmAT07tA",
+      [new Ingredient("Salad", 100), new Ingredient("Tomato", 1)]
     ),
     new Recipe(
-      "Second Recipe",
+      "Pizza",
       "This is a test",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-E28BB6cRRotXBHjM87qcwyaMg6UGdc_N0J_TBk62jnVmAT07tA"
+      "https://www.wholesomeyum.com/wp-content/uploads/2018/01/wholesomeyum-stuffed-spaghetti-squash-lasagna-boats-recipe-with-meat-2.jpg",
+      [new Ingredient("Bread", 1), new Ingredient("Chease", 100)]
     )
   ];
 
