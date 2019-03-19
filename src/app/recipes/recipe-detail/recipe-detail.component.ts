367cfb9b7e3f65b499aef4145eb35c388516e7bf
vladilin <ilin96inbox@gmail.com>
2019-03-19 16:26:24 +0300

    recipe delete method

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -31,7 +31,8 @@ export class RecipeDetailComponent implements OnInit {
     this.router.navigate(["edit"], { relativeTo: this.route });
   }
 
-  onDeleteRecipe(){
-    this.recipeService.deleteRecipe(this.id)
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(["/recipes"]);
   }
 }
