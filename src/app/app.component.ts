80fcffceafeb525558977ff4d689ff10a5063dc9
vladilin <ilin96inbox@gmail.com>
2019-03-20 09:16:19 +0300

    add firebase auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = "recipe";
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    firebase.initializeApp({
+      apiKey: "AIzaSyA0Ib1vAW4MB4KdDuG6tcmQ151qky0wnUU",
+      authDomain: "recipe-a4b60.firebaseapp.com"
+    });
+  }
+
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class SignUpService {
+  signupUser(email: string, password: string);
+}
