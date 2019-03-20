09ba4b8727d56c2d72003f04843cffe22263a9b8
vladilin <ilin96inbox@gmail.com>
2019-03-20 09:10:30 +0300

    created sign up component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 
 // import { PageNotFoundComponent } from './';
 
@@ -21,7 +22,8 @@ const routes: Routes = [
       { path: ":id/edit", component: RecipeEditComponent }
     ]
   },
-  { path: "shopping-list", component: ShoppingListComponent }
+  { path: "shopping-list", component: ShoppingListComponent },
+  { path: "signup", component: SignupComponent }
 
   // { path: "**", component: PageNotFoundComponent }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from "./recipes/recipe.service";
 import { HttpModule } from "@angular/http";
 import { DataStorageService } from "./shared/data-storage.service";
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,9 @@ import { DataStorageService } from "./shared/data-storage.service";
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
